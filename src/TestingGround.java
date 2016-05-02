d783bc5a4c59f8c65b50d4203c288ba7f8706355
wubalubadubdub <beargrillsllc@gmail.com>
2016-05-01 23:33:47 -0400

    implemented generic min method

--- src/TestingGround.java
+++ src/TestingGround.java
@@ -35,8 +35,22 @@ public class TestingGround {
         madMen.add(bertCooper);
 
 
-        final Person youngestCastMember = (Person) min(madMen, new AgeComparator());
+        final Person youngestCastMember = min(madMen, new AgeComparator());
         System.out.println(youngestCastMember);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+
+        // System.out.println(min(numbers, (n1, n2) -> Integer.compare(n1, n2))); // option 1
+        // compareTo is called on one Integer, takes another as a parameter, and returns
+        // another Integer, so it fits the (n1, n2) -> n1.compare(n2) type seen above
+
+        System.out.println(min(numbers, Integer::compare)); // java 8 version
+        // Integer.compare is an implementation of Comparator interface's compare method
+
+
 
         // want to write the min method generically, so it can find min age of
         // People, or min number of some Number class, or whatever.
@@ -64,17 +78,25 @@ public class TestingGround {
 
     }
 
-    public static Object min(List values, Comparator comparator) {
+    // type parameter goes between method modifier declarations and before return type
+    // this type parameter T is limited to this one method, since we didn't declare it with class name
+    // the <T> declaration for the method means that we will use generic type parameter T in the
+    // method. It does not mean that the returns type T. public static <T> Object returns an Object,
+    // not a T. However public static <T> T is both parameterized by type T and returns type T.
+
+    // the List and Comparator passed in must now be of the same type. above, we pass in a
+    // List<Person> and a Comparator<Person>, so this is fine.
+    public static <T> T min(List<T> values, Comparator<T> comparator) {
 
         if (values.isEmpty()) {
 
             throw new IllegalArgumentException("List is empty. Cannot find min.");
         }
 
-        Object lowestElement = values.get(0);
+        T lowestElement = values.get(0);
 
         for (int i = 1; i < values.size(); i++) {
-            final Object element = values.get(i);
+            final T element = values.get(i);
 
             if (comparator.compare(element, lowestElement) < 0) { // current element is less than previous low
                 lowestElement = element;
